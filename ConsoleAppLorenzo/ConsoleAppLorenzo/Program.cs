4d5f821fce375e6e8cee21765c8715bfe529745d
Anthony Cavallaro <anthocava97@gmail.com>
2024-10-15 09:35:12 +0200

    Project setup + first program

---
+++ ConsoleAppLorenzo/ConsoleAppLorenzo/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Inserisci la base del triangolo rettangolo:");
+double bSide = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Inserisci l'altezza del triangolo rettangolo:");
+double hSide = Convert.ToDouble(Console.ReadLine());
+
+double hypotenuse = Math.Sqrt(Math.Pow(bSide, 2) + Math.Pow(hSide, 2));
+
+Console.WriteLine($"L'ipotenusa del triangolo rettangolo è: {hypotenuse}");
+
